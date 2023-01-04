11e3d75e44098916e4a6ce3a532c45969a85af2d
Emmaline <emmalinerose19@gmail.com>
2023-01-04 16:49:23 -0600

    connected planetscale safely

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-// const port = process.env.PORT || 
+const port = process.env.PORT
 
 require('dotenv').config()
 
 
 const mysql = require('mysql2')
 const connection = mysql.createConnection(process.env.DATABASE_URL)
-
 console.log('Connected to PlanetScale!')
 connection.end()
+
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(port, () => console.log(`SERVER RUNNING ON ${port}`))
+
+
 
